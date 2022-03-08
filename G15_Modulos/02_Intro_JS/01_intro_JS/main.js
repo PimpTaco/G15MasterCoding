f22316b86e27bc267b0b1b7c858e9f3e3c7be902
Manuel Sebastian <seferinoubago12@gmail.com>
2022-03-07 20:24:41 -0600

    primermodulo

---
+++ G15_Modulos/02_Intro_JS/01_intro_JS/main.js
@@ -0,0 +1,19 @@
+
+
+ //tipo numerico
+ var num1 = 10
+
+ //tipo texto
+ var saludo = "hola mundo"
+
+ //tipo booleano-
+ var verdadero = false //true
+ 
+ var num2 = 20
+
+ num1 + num2 
+
+
+ //salida de datos o informacion
+ console.log(datos que quiero ver)
+ alert(datos que quiero ver)
